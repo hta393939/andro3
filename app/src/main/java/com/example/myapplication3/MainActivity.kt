1ef091b3b9ca2d19e582cf1ae928b3d8e88fea4e
hta393939 <hta393939@gmail.com>
2026-02-17 20:07:53 +0900

    commit

--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -43,6 +44,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "あるえ",
             modifier = modifier.fillMaxWidth()
         )
+        Row(modifier = Modifier) {
+            Text(
+                text = "有効",
+                modifier = modifier
+            )
+        }
     }
 }
 
